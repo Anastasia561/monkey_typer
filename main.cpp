243cf6a20fd09fd0b671b05c9a6666527907a387
Anastasiia Kurulenko <kurulenkoanastasia@gmail.com>
2024-04-29 17:35:11 +0200

    implemented text moving

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
-#include <iostream>
+#include <fmt/core.h>
+#include <SFML/Graphics.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+auto main() -> int {
+    auto window = sf::RenderWindow(
+            sf::VideoMode(800,600),
+            "game",
+            sf::Style::Default,
+            sf::ContextSettings(0,0,8)
+            );
+
+    auto font = sf::Font();
+    if (!font.loadFromFile("OpenSans.ttf")){
+        fmt::println("{}", "Error while loading font");
+    }
+
+    auto text = sf::Text();
+
+    text.setFont(font);
+    text.setString("Hello world");
+    text.setCharacterSize(24);
+    text.setFillColor(sf::Color::Red);
+    text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+//    auto circle = sf::CircleShape(100, 100);
+//    circle.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()){
+        for(auto event=sf::Event(); window.pollEvent(event);){
+            if(event.type==sf::Event::Closed){
+                window.close();
+            }
+        }
+
+
+
+        text.move(0.01, 0);
+
+        window.clear(sf::Color::White);
+        window.draw(text);
+        window.display();
+    }
+
 }
